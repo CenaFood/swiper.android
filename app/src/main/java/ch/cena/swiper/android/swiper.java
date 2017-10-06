3bdeaf7e239178c0eb0c24ed080da248797c872d
Philipp Lüthi <philipp@luethi.rocks>
2017-10-06 16:21:02 +0200

    Bugfix & Made my first activity

--- app/src/main/java/ch/cena/swiper/android/swiper.java
+++ app/src/main/java/ch/cena/swiper/android/swiper.java
@@ -1,13 +1,23 @@
 package ch.cena.swiper.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class swiper extends AppCompatActivity {
+
+    public static final String EXTRA_USER_NAME = "ch.cena.swiper.android.EXTRA_USER_NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swiper);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra(EXTRA_USER_NAME);
+
+        TextView textView = (TextView) findViewById(R.id.txtUsername);
+        textView.setText(userName);
     }
 }
--- app/src/main/java/ch/cena/swiper/android/welcome.java
+++ app/src/main/java/ch/cena/swiper/android/welcome.java
@@ -1,7 +1,10 @@
 package ch.cena.swiper.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class welcome extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void startLogin(View view){
+        Intent intent = new Intent(this, swiper.class);
+        EditText editText = (EditText) findViewById(R.id.txtUserName);
+        String userName = editText.getText().toString();
+        intent.putExtra(swiper.EXTRA_USER_NAME,userName);
+        startActivity(intent);
+
     }
 }
